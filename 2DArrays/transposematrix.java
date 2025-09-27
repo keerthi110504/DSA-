f615c46f29ff4350f23868ee7c13d914173fb653
keerthi110504 <keerthimuthyala151@gmail.com>
2025-09-27 22:05:38 +0530

    transposematrix

--- 2DArrays/transposematrix.java
+++ 2DArrays/transposematrix.java
@@ -1,7 +1,19 @@
-class transposematrix
-{
-    public static void main(String  args[])
-    {
+class Solution {
+    public ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
+        // code here
+        int n=mat.length;
+        int m=mat[0].length;
+         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            ArrayList<Integer>row = new ArrayList<>();
+            for(int j=0;j<m;j++)
+            {
+                row.add(mat[j][i]);
+            }
+            result.add(row);
+        }
+        return result;
         
     }
-}
+}s
