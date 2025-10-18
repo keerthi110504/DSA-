9ad974d7781920a8b4590e7a34fccb56f9a70026
keerthi110504 <keerthimuthyala151@gmail.com>
2025-10-18 22:18:46 +0530

    countingbits

---
+++ bitmanipulation/countingbits.java
@@ -0,0 +1,21 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+}
