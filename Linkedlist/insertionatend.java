45016b0bb5397c5fad37ffe12ef1d7c03f2900ab
keerthi110504 <keerthimuthyala151@gmail.com>
2025-10-28 20:35:49 +0530

    adding a node at end of LL

---
+++ Linkedlist/insertionatend.java
@@ -0,0 +1,44 @@
+// You are given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the head of the modified Linked List.
+
+// Examples :
+
+// Input: x = 6,
+   
+// Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+// Explanation: We can see that 6 is inserted at the end of the linkedlist.
+   
+// Input: x = 1,
+   
+// Output: 4 -> 5 -> 1
+// Explanation: We can see that 1 is inserted at the end of the linked list.
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        Node newnode=new Node(x);
+        if (head == null) {
+            return newnode;
+        }
+        Node temp = head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next = newnode;
+        
+        return head;
+        
+    }
+}
